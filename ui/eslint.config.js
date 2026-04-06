53a9501ce627e6512578849a015e50efa09edc66
NNNAZHAN <nnnazhan@MBN9-NNNAZHAN.local>
2026-04-06 10:24:28 +0800

    Add Prettier formatting to UI with ESLint integration

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,10 +16,17 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig, // 👈 disables conflicting ESLint rules
     ],
+    plugins: {
+      prettier: prettierPlugin, // 👈 runs Prettier as an ESLint rule
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      'prettier/prettier': 'warn', // 👈 shows Prettier issues as warnings
     },
   },
 ])
